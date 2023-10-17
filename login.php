f302680f9e675ab68a859efb052a5610d6d606db
UPHMC - Chasey Larrisse V. Elizarde <phmc.itd.2023@gmail.com>
2023-10-17 11:12:03 +0800

    Added show password button for login

--- login.php
+++ login.php
@@ -7,11 +7,6 @@ Description: Frontend SMS  -->
 <head>
     <meta charset="UTF-8">
     <title>Doctors Fee System</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer">
     <link rel="stylesheet" href="style.css">
 </head>
 
