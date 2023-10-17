4672bc69638c2825b70b5661124219793b91cebe
UPHMC - Chasey Larrisse V. Elizarde <phmc.itd.2023@gmail.com>
2023-10-17 11:07:33 +0800

    Added navbar

---
+++ nav/navbar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <ul class="navigation">
+                <li class="navlogout"><a href="logout.php">LOGOUT</a></li>
+                <li class="navlist"><a href="main.php">HOME</a></li>
+                <li class="navlist"><a href="payment.php">PAYMENT</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
